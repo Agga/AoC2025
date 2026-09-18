d7bcac93729eb3b82b0187dfba9a2b95d01938f6
Adrian Lück <alueck@gm.lan>
2026-09-18 16:51:39 +0200

    day3 start

--- day03/main.rs
+++ day03/main.rs
@@ -1,4 +1,7 @@
-use std::time::Instant;
+use std::{
+    cmp::Ordering::{self, Equal, Greater, Less},
+    time::Instant,
+};
 
 #[allow(dead_code)]
 const INPUT_DATA: &str = include_str!("input.txt");
@@ -6,12 +9,37 @@ const INPUT_DATA: &str = include_str!("input.txt");
 const TEST_DATA: &str = include_str!("test.txt");
 
 #[allow(unused_variables)]
-pub fn do_part1(input: &str) -> usize {
-    0
+pub fn do_part1(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| {
+            let nums: Vec<u64> = line.chars().map(|c| c as u64 - 48).collect();
+
+            let left = nums
+                .iter()
+                .take(nums.len() - 1)
+                .enumerate()
+                .max_by(|a, b| match a.1.cmp(b.1) {
+                    Ordering::Less => Less,
+                    _ => Greater,
+                })
+                .unwrap();
+
+            let right = nums
+                .iter()
+                .enumerate()
+                .skip(left.0 + 1)
+                .max_by(|a, b| a.1.cmp(b.1))
+                .unwrap();
+
+            let joltage = left.1 * 10 + right.1;
+            joltage
+        })
+        .sum()
 }
 
 #[allow(unused_variables)]
-pub fn do_part2(input: &str) -> usize {
+pub fn do_part2(input: &str) -> u64 {
     0
 }
 
@@ -27,10 +55,10 @@ fn main() {
 
 #[test]
 fn part1() {
-    assert_eq!(0, do_part1(TEST_DATA));
+    assert_eq!(do_part1(TEST_DATA), 357);
 }
 
 #[test]
 fn part2() {
-    assert_eq!(0, do_part2(TEST_DATA));
+    assert_eq!(do_part2(TEST_DATA), 0);
 }
